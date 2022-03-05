64864d7fcc20eb7ca107fe2a4af9e1c9a902256b
DeathVenom54 <fdragon602@gmail.com>
2022-03-05 18:10:15 +0530

    app complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,26 @@
 import express from 'express';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+
+let count = 0;
 
 const app = express();
 
+// middleware to ward off hax0rs
+app.use(helmet());
+app.use(
+  rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 100,
+    message: 'Too many requests, please try again after 5 minutes',
+    standardHeaders: true,
+    legacyHeaders: false,
+  })
+);
+
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.send(`This page has been viewed ${++count} times`);
 });
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
